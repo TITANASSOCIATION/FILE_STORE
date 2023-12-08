e2b3e0aca0e3ff90ae91375b2902a28ac6b4072b
TITAN ASSOCIATION <153067412+TITANASSOCIATION@users.noreply.github.com>
2023-12-08 09:10:35 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -110,7 +110,7 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
+                    InlineKeyboardButton("😊 About Me", callback_data = "premium"),
                     InlineKeyboardButton("🔒 Close", callback_data = "close")
                 ]
             ]
