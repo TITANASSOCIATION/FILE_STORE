f0be83690d6c0a45b0dc922a57328fe5a1ec28b7
TITAN ASSOCIATION <153067412+TITANASSOCIATION@users.noreply.github.com>
2023-12-08 12:36:12 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -18,37 +18,6 @@ the user name is of the user = {username}
 the first name of the user = {user}
 the last name of the user = {last}"""
 
-@Bot.on_callback_query()
-async def cb_handler(client: Bot, query: CallbackQuery):
-    data = query.data
-    if data == "hey":
-        await query.message.edit_photo(
-            photo="https://telegra.ph/file/e0a0622cab9fd1a001552.jpg",
-            caption="hey",
-            reply_markup = InlineKeyboardMarkup(
-                 [
-                    [
-                        InlineKeyboardButton("🔒 Close", callback_data = "close")
-                    ]
-                ]
-            )
-        )
-    elif data == "lol":
-         await query.message.edit_photo(
-            photo="https://telegra.ph/file/e0a0622cab9fd1a001552.jpg",
-            caption="lund",
-            reply_markup = InlineKeyboardMarkup(
-                 [
-                    [
-                        InlineKeyboardButton("🔒 Close", callback_data = "close")
-                    ]
-                ]
-            )
-        )
-        
-        
-
-
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
 async def start_command(client: Client, message: Message):
     id = message.from_user.id
@@ -140,7 +109,7 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data = "hey"),
+                    InlineKeyboardButton("😊 About Me", callback_data = "premium"),
                     InlineKeyboardButton("🔒 Close", callback_data = "lol")
                 ]
             ]
