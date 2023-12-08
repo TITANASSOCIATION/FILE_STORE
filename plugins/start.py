67b100bedae639ca2572f0a86e21858ecfa42c74
TITAN ASSOCIATION <153067412+TITANASSOCIATION@users.noreply.github.com>
2023-12-08 15:38:13 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -288,7 +288,5 @@ Unsuccessful: <code>{unsuccessful}</code></b>"""
 
 @Bot.on_message(filters.command('restart') & filters.private & filters.user(ADMINS))
 async def stop_button(bot, message):
-    msg = await bot.send_message(text="**🔄 𝙿𝚁𝙾𝙲𝙴𝚂𝚂𝙴𝚂 𝚂𝚃𝙾𝙿𝙴𝙳. 𝙱𝙾𝚃 𝙸𝚂 𝚁𝙴𝚂𝚃𝙰𝚁𝚃𝙸𝙽𝙶...**", chat_id=message.chat.id)       
-    await asyncio.sleep(3)
-    await msg.edit("**✅️ 𝙱𝙾𝚃 𝙸𝚂 𝚁𝙴𝚂𝚃𝙰𝚁𝚃𝙴𝙳. 𝙽𝙾𝚆 𝚈𝙾𝚄 𝙲𝙰𝙽 𝚄𝚂𝙴 𝙼𝙴**")
-    os.execl(sys.executable, sys.executable, *sys.argv)
+    await bot.send_message(message.chat.id, "Restarting...")
+    os.system("heroku restart -a filestoretitantestindia")
